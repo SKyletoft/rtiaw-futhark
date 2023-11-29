ba9f4cad194411bf853fa93dd9993468a6f68072
Samuel Kyletoft <samuel@kyletoft.se>
2023-11-29 21:41:09 +0100

    Raytracing in a weekend start

--- src/lib.fut
+++ src/lib.fut
@@ -1,1 +1,41 @@
-entry hi: []f64 -> []f64 = map (*2)
+type Pixel = {r: u8, g: u8, b: u8}
+
+def black: Pixel = {r = 0, g = 0, b = 0}
+def black_image (h: i64) (w: i64): [h][w]Pixel =
+  black
+  |> replicate w
+  |> replicate h
+
+def transform_pixel (lim: i64) (h: i64) (w: i64): Pixel = 
+  let conv_colour a =
+    a
+    |> f32.i64
+    |> (/ f32.i64 lim)
+    |> (* 255.9999)
+    |> u8.f32
+  in {
+    r = conv_colour h,
+    g = conv_colour w,
+    b = conv_colour ((h * w) % lim)
+  }
+
+def create_image (f: i64 -> i64 -> Pixel) (x: i64) (y: i64): [x][y]Pixel =
+  let line = iota y
+  let lines = replicate x line
+  let inner ((i, js): (i64, []i64)): []Pixel = map (f i) js
+  in zip (iota x) lines |> map inner
+
+def colour_image (h: i64) (w:i64): [h][w]Pixel =
+  create_image (transform_pixel h) h w
+
+def pixel_to_arr (pixel: Pixel) = [pixel.r, pixel.g, pixel.b]
+
+def flatten_pixels (pxs: [][]Pixel): []u8 =
+  pxs
+  |> flatten
+  |> map pixel_to_arr
+  |> flatten
+
+entry calc (h: i64) (w: i64) =
+  colour_image h w
+  |> flatten_pixels
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
+use std::fmt;
+
 pub mod fut;
+
+const HEIGHT: usize = 512;
+const WIDTH: usize = 512;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, PartialEq)]
+struct Colour {
+	red: u8,
+	green: u8,
+	blue: u8,
+}
+
+impl fmt::Display for Colour {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+	    let Colour { red, green, blue } = *self;
+            write!(f, "{red} {green} {blue}")
+    }
+}
 
 fn main() {
 	let res = with_futhark().unwrap();
-	println!("{:#?}", res);
+	assert_eq!(res.len(), HEIGHT * WIDTH);
+	println!("P3\n{WIDTH} {HEIGHT}\n255");
+	for line in res.into_iter() {
+		println!("{line}");
+	}
 }
 
-fn with_futhark() -> Result<Vec<f64>, fut::Error> {
+fn with_futhark() -> Result<Vec<Colour>, fut::Error> {
 	let ctx = fut::Context::new()?;
 
-	let data = (1..=10).map(|i| i as f64).collect::<Vec<_>>();
-	let arr = fut::ArrayF64D1::new(&ctx, [data.len() as _], &data)?;
+	let height = HEIGHT as _;
+	let width = WIDTH as _;
 
-	let res = ctx.hi(&arr)?;
+	let res = ctx
+		.calc(height, width)?
+		.get()?
+		.chunks(3)
+		.map(|arr| Colour {
+			red: arr[0],
+			green: arr[1],
+			blue: arr[2],
+		})
+		.collect();
 
-	res.get()
+	Ok(res)
 }
