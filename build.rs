0c6e6f076c724b0aa4e6647b0a0a134b50cc956d
Samuel Kyletoft <samuel@kyletoft.se>
2023-12-15 06:10:24 +0100

    Replace C with threaded C for debug builds

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() {
 	println!("cargo:rerun-if-changed=src/raytracing.fut");
 
 	#[cfg(debug_assertions)]
-	futhark_bindgen::build(futhark_bindgen::Backend::C, "src/lib.fut", "fut.rs");
+	futhark_bindgen::build(futhark_bindgen::Backend::Multicore, "src/lib.fut", "fut.rs");
 	#[cfg(not(debug_assertions))]
 	futhark_bindgen::build(futhark_bindgen::Backend::HIP, "src/lib.fut", "fut.rs");
 }
