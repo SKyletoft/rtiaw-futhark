9c457e0bbf5bec4f687aeaeb4f3593f26bbf4b13
Samuel Kyletoft <samuel@kyletoft.se>
2023-12-18 22:52:53 +0100

    Build.rs: Update list of (futhark) dependencies

--- build.rs
+++ build.rs
@@ -1,8 +1,12 @@
 fn main() {
-	println!("cargo:rerun-if-changed=src/lib.fut");
 	println!("cargo:rerun-if-changed=src/colour.fut");
-	println!("cargo:rerun-if-changed=src/vector.fut");
+	println!("cargo:rerun-if-changed=src/interval.fut");
+	println!("cargo:rerun-if-changed=src/lib.fut");
+	println!("cargo:rerun-if-changed=src/option.fut");
+	println!("cargo:rerun-if-changed=src/random.fut");
 	println!("cargo:rerun-if-changed=src/raytracing.fut");
+	println!("cargo:rerun-if-changed=src/tuple.fut");
+	println!("cargo:rerun-if-changed=src/vector.fut");
 
 	#[cfg(debug_assertions)]
 	futhark_bindgen::build(futhark_bindgen::Backend::Multicore, "src/lib.fut", "fut.rs");
